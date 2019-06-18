c93ee3d77d2c843e9e62237c2a33e9be68863f8b
Jinchen Hu <jinchen.hu22@gmail.com>
2019-06-17 20:23:20 -0400

    disclaimer

---
+++ a4q3_Disclaimer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Privacy/Disclaimer Statement</title>
+    <style>
+        body{
+            background-color: #ffffcc;
+        }
+    </style>
+</head>
+<body>
+    <?php include("a4q3_header.php"); ?>
+    <p><h4>Privacy Policy</h4>
+    We have put in place appropriate security measures to prevent your personal data from being
+    accidentally lost, used or accessed in an unauthorised way, altered or disclosed. In addition,
+    we limit access to your personal data to those employees, agents, contractors and other third parties
+    who have a business need to know. They will only process your personal data on our instructions and they
+    are subject to a duty of confidentiality.<br/><br/>
+    We have put in place procedures to deal with any suspected personal data breach and will notify you and any
+    applicable regulator of a breach where we are legally required to do so.<br/><br/>
+    You information will never be sold or misused to another parties.
+    </p>
+
+    <p><h4>Disclaimer</h4>
+        The information on this website is provided by HOTEL RESERVATION. While we endeavor to keep the information up
+        to date and correct, we make no representations or warranties of any kind, express or implied, about the
+        completeness, accuracy, reliability, suitability or availability with respect to the website or the
+        information, products, services, or related graphics contained on the website for any purpose.
+        Any reliance you place on such information is therefore strictly at your own risk.<br/><br/>
+        In no event will we be liable for any loss or damage including without limitation,
+        indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss
+        of data or profits arising out of, or in connection with, the use of this website.</p>
+    <?php include('a4q3_footer.php'); ?>
+</body>
+</html>

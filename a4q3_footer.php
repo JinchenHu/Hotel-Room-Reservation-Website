925404a5b85bedee4a6d4a6d2c2e66d04fd75610
Jinchen Hu <jinchen.hu22@gmail.com>
2019-06-17 20:23:27 -0400

    footer

---
+++ a4q3_footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>footer</title>
+        <meta charset="UTF-8">
+        <style>
+            span{
+                background-color: #ddeeff;
+                color: black;
+            }
+        </style>
+    </head>
+    <body>
+        <span><a href="a4q3_Disclaimer.php" target="_blank"><strong>Privacy/Disclaimer Statement</strong></a></span>
+    </body>
+</html>

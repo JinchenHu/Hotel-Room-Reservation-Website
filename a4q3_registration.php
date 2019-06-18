bcb17264dad5f7df78a834aa865ebe48d97e638c
Jinchen Hu <jinchen.hu22@gmail.com>
2019-06-17 20:25:28 -0400

    Create a4q3_registration.php

---
+++ a4q3_registration.php
@@ -0,0 +1,28 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luke
+ * Date: 18-Apr-19
+ * Time: 19:30
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registration</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <?php include("a4q3_header.php"); ?>
+    <?php
+        echo "<p><h4>Congratulations! Your account is created successfully</h4></p>";
+    ?>
+    <a href="a4q3.php" style="text-decoration: none">
+        <button type="button">back to the search page</button>
+    </a><br/><br/>
+    <?php include('a4q3_footer.php'); ?>
+</body>
+</html>
